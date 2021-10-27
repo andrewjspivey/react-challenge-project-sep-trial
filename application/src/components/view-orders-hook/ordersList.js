6e83259de71a65458050238bf96438e9433ca9d2
andrewjspivey <12spivey@gmail.com>
2021-10-27 14:29:37 -0700

    feat(orderList+viewOrders): add delete order functionality

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {convertUnixtoHHMMSS} from '../../../src/utils/timeFormat'
+import { SERVER_IP } from '../../private';
 
 const OrdersList = (props) => {
-    const { orders } = props;
+    const { orders, setOrders } = props;
     if (!props || !props.orders || !props.orders.length) return (
         <div className="empty-orders">
             <h2>There are no orders to display</h2>
         </div>
     );
+
+        const deleteOrder = (orderId) => {
+        fetch(`${SERVER_IP}/api/delete-order`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: orderId
+            })
+        }).then(res => {
+            setOrders(orders.filter(order => order._id !== orderId)
+            )
+        }).catch(error => console.log(error))
+    }
 
     return orders.map(order => {
         const createdDate = new Date(order.createdAt);
@@ -23,7 +39,7 @@ const OrdersList = (props) => {
                 </div>
                 <div className="col-md-4 view-order-right-col">
                     <button className="btn btn-success">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+                    <button onClick={() => deleteOrder(order._id)} className="btn btn-danger">Delete</button>
                 </div>
             </div>
         );
