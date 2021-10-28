e6c9b41af043651e2ec6bfb77b0bebeb855ac10b
andrewjspivey <12spivey@gmail.com>
2021-10-28 14:42:25 -0700

    feat(nav):adds username to navbar if logged in

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/actions/authActions"
 
 const Nav = (props) => {
     const dispatch = useDispatch()
+    const auth = useSelector(state => state.auth)
     return (
         <div className="nav-strip">
             <Link to={"/order"} className="nav-link">
@@ -18,11 +19,18 @@ const Nav = (props) => {
                     <label className="nav-label">View Orders</label>
                 </div>
             </Link>
-            <Link to={"/login"} onClick={() => dispatch(logoutUser())} className="nav-link">
+            <Link to={"/login"} onClick={() => dispatch(logoutUser())} className="nav-link" id="middle-link">
                 <div className="nav-link-style">
                     <label className="nav-label">Log Out</label>
                 </div>
             </Link>
+            {auth.email !== null &&
+            <div className="nav-link" id="middle-link">
+                <div className="nav-link-style">
+                    <label className="nav-label">{auth.email}</label>
+                </div>
+            </div>
+            }
         </div>
     );
 }
