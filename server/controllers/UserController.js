8073b8d2edf0ce87e596ba5fe4d413f289e259fc
andrewjspivey <12spivey@gmail.com>
2021-11-04 16:06:08 -0700

    feat(login-system): adds login with jtw token and calls redux action loginUser

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
 const User = require("../models/userModel");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 
 module.exports = {
   getUsers: async (req, res) => {
@@ -18,18 +20,24 @@ module.exports = {
       if (!email || !password) {
         return res.status(400).json({ msg: "Enter all fields!" });
       }
+
+      if (password.length < 6)
+      return res
+        .status(400)
+        .json({ msg: "Password needs to be at least 6 characters long!" });
+
       const existingUser = await User.findOne({ email: email });
       if (existingUser) {
         return res.status(400).json({ msg: "User with email already exists!" });
       }
 
-      //   const salt = await bcrypt.genSalt();
-      //   const passHash = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt();
+        const passHash = await bcrypt.hash(password, salt);
 
       const newUser = new User({
         email,
-        // password: passHash,
-        password,
+        password: passHash,
+        // password,
       });
       const savedUser = await newUser.save();
 
@@ -54,16 +62,18 @@ module.exports = {
           .status(400)
           .json({ msg: "No account with this email has been registered!" });
       }
-
-      // const isMatch = await bcrypt.compare(password, user.password);
-      // if (!isMatch) {
-      //   res.status(400).json({ msg: "Invalid credentials!" });
-      // }
-      res
-        .status(200)
-        .json({ success: true, email: email, token: "12345luggage" });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        res.status(400).json({ msg: "Invalid credentials!" });
+      }
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+      res.status(200).json({ success: true, email: email, token: token });
     } catch (error) {
-      res.status(500).json({ success: false, error: "Unknown error" });
+      res
+        .status(500)
+        .json({ success: false, error: "Unknown error (status 500)" });
     }
   },
 };
